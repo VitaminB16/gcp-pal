d35786428e93c9bc895cbe7228f2ed3bf3becdc3
VitaminB16 <artemiy.nosov@gmail.com>
2024-03-15 18:07:54 +0000

    chore: tests for PubSub

---
+++ tests/test_pubsub.py
@@ -0,0 +1,22 @@
+import pytest
+
+from gcp_tools.pubsub import PubSub
+
+
+@pytest.fixture
+def mocker():
+    from unittest.mock import patch
+
+    with patch("google.cloud.pubsub_v1.PublisherClient") as publisher:
+        yield publisher
+
+
+def test_pubsub_publish(mocker):
+    publisher = mocker
+    p = PubSub("test_topic")
+    p.publish("data")
+    publisher.assert_called_once()
+    publisher.return_value.publish.assert_called_once()
+    publisher.return_value.publish.assert_called_with(
+        p.topic_path, "data".encode("utf-8")
+    )
