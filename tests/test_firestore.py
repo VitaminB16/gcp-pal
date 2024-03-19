bd6a05e6109d899e287e745e1dbaffab37bdcb5c
VitaminB16 <artemiy.nosov@gmail.com>
2024-03-19 00:15:44 +0000

    tests: Firestore tests independence

--- tests/test_firestore.py
+++ tests/test_firestore.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+from uuid import uuid4
 from google.cloud import firestore
 
 from gcp_tools.firestore import Firestore
@@ -16,9 +17,10 @@ def test_write_dict():
         "c": [1, 2, 3],
     }
     collection_name = "test_write_dict"
-    Firestore(f"{collection_name}/test_document").write(data)
+    document_name = f"test_document_{uuid4()}"
+    Firestore(f"{collection_name}/{document_name}").write(data)
     firestore_client = firestore.Client()
-    doc_ref = firestore_client.collection(collection_name).document("test_document")
+    doc_ref = firestore_client.collection(collection_name).document(document_name)
     written_data = doc_ref.get().to_dict()
     success = dicts_equal(written_data, data)
     # Clean up
@@ -33,10 +35,11 @@ def test_write_df():
         "c": [1, 2, 3],
     }
     collection_name = "test_write_df"
+    document_name = f"test_document_{uuid4()}"
     df = pd.DataFrame(data)
-    Firestore(f"{collection_name}/test_document").write(df)
+    Firestore(f"{collection_name}/{document_name}").write(df)
     firestore_client = firestore.Client()
-    doc_ref = firestore_client.collection(collection_name).document("test_document")
+    doc_ref = firestore_client.collection(collection_name).document(document_name)
     written_data = doc_ref.get().to_dict()
     expected_metadata = {
         "dtypes": {"a": "int64", "b": "object", "c": "int64"},
@@ -48,11 +51,9 @@ def test_write_df():
         "c": {"0": 1, "1": 2, "2": 3},
     }
     expected_output = {"data": expected_data, "metadata": expected_metadata}
-    print(written_data)
-    print(expected_output)
     success = dicts_equal(written_data, expected_output)
     # Clean up
-    # doc_ref.delete()
+    doc_ref.delete()
     assert success
 
 
@@ -63,10 +64,11 @@ def test_read_dict():
         "c": [1, 2, 3],
     }
     collection_name = "test_read_dict"
+    document_name = f"test_document_{uuid4()}"
     firestore_client = firestore.Client()
-    doc_ref = firestore_client.collection(collection_name).document("test_document")
+    doc_ref = firestore_client.collection(collection_name).document(document_name)
     doc_ref.set(data)
-    read_data = Firestore(f"{collection_name}/test_document").read()
+    read_data = Firestore(f"{collection_name}/{document_name}").read()
     success = dicts_equal(read_data, data)
     # Clean up
     doc_ref.delete()
@@ -84,10 +86,11 @@ def test_read_df():
         "c": {"0": 1, "1": 2, "2": 3},
     }
     collection_name = "test_read_df"
+    document_name = f"test_document_{uuid4()}"
     firestore_client = firestore.Client()
-    doc_ref = firestore_client.collection(collection_name).document("test_document")
+    doc_ref = firestore_client.collection(collection_name).document(document_name)
     doc_ref.set({"data": data, "metadata": metadata})
-    read_data = Firestore(f"{collection_name}/test_document").read(apply_schema=True)
+    read_data = Firestore(f"{collection_name}/{document_name}").read(apply_schema=True)
     expected_data = pd.DataFrame(data).reset_index(drop=True)
     success = read_data.equals(expected_data)
     # Clean up
@@ -97,10 +100,11 @@ def test_read_df():
 
 def test_delete():
     collection_name = "test_delete"
+    document_name = f"test_document_{uuid4()}"
     firestore_client = firestore.Client()
-    doc_ref = firestore_client.collection(collection_name).document("test_document")
+    doc_ref = firestore_client.collection(collection_name).document(document_name)
     doc_ref.set({"a": 1})
-    Firestore(f"{collection_name}/test_document").delete()
+    Firestore(f"{collection_name}/{document_name}").delete()
     read_data = doc_ref.get().to_dict()
     success = read_data is None
     # Clean up
@@ -126,8 +130,10 @@ def test_read_collection():
     success2 = all(isinstance(output[x], pd.DataFrame) for x in output)
     # Clean up
     Firestore(collection_name).delete()
+    success3 = Firestore(collection_name).ls() == []
     assert success1
     assert success2
+    assert success3
     assert output["test_document1"].equals(data)
     assert output["test_document2"].equals(data)
     assert output["test_document3"].equals(data)
