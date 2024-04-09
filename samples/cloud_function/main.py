f171e5e69646eb66265a93e49f89c1dfcfeaae22
VitaminB16 <artemiy.nosov@gmail.com>
2024-04-09 17:28:35 +0100

    chore: Tidy

--- samples/cloud_function/main.py
+++ samples/cloud_function/main.py
@@ -1,3 +1,4 @@
+import json
 from function import square_root
 
 
@@ -5,4 +6,10 @@ def entry_point(request):
     data = request.get_json()
     if data is None:
         data = request.form
-    return square_root(**data)
+    data = json.loads(data)
+    print(f"Data: {data}")
+    print(f"Type: {type(data)}")
+    if isinstance(data, dict):
+        data = data.get("data")
+    data = float(data)
+    return square_root(data)
