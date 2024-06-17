363d18a90ee06589ee72bc85662a0904624e0f73
VitaminB16 <artemiy.nosov@gmail.com>
2024-06-17 12:37:19 +0100

    tests: PubSub

--- tests/test_pubsub.py
+++ tests/test_pubsub.py
@@ -11,6 +11,34 @@ def mocker():
         yield publisher
 
 
+def test_pubsub_constructor():
+    success = {}
+
+    success[0] = PubSub().level == "project"
+    success[1] = PubSub("projects/my-project").level == "project"
+    success[2] = PubSub("projects/my-project").path == "my-project"
+    success[3] = PubSub("projects/my-project").project == "my-project"
+    success[4] = PubSub("projects/my-project/topics/my-topic").level == "topic"
+    success[5] = (
+        PubSub("projects/my-project/topics/my-topic").path == "my-project/my-topic"
+    )
+    success[6] = PubSub("projects/my-project/topics/my-topic").project == "my-project"
+    success[7] = PubSub("projects/my-project/topics/my-topic").topic_id == "my-topic"
+
+    success[8] = PubSub("my-project/my-topic").topic_id == "my-topic"
+    success[9] = PubSub("my-project/my-topic").project == "my-project"
+    success[10] = PubSub("my-project/my-topic").path == "my-project/my-topic"
+    success[11] = PubSub("my-project/my-topic").level == "topic"
+
+    success[11] = PubSub("my-project").level == "project"
+    success[12] = PubSub("my-project").project == "my-project"
+    success[13] = PubSub("my-project").path == "my-project"
+
+    failed = [k for k, v in success.items() if not v]
+
+    assert not failed
+
+
 def test_pubsub_publish(mocker):
     publisher = mocker
     p = PubSub("test_topic")
